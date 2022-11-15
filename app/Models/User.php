946cb0bc936363c8e3b9e0d6281c86c85d6d3232
mywu57 <my.wu579@gmail.com>
2022-11-16 00:20:59 +0700

    installation

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Ophim\Core\Models\User as OphimUser;
 
-class User extends Authenticatable
+class User extends OphimUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
